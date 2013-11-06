8f0fc8719f74eb66109d25bf3ea93d357243f752
Julien LEICHER <jleicher@gmail.com>
2013-11-06 22:01:01 +0100

    Updated imports

--- beard.py
+++ beard.py
@@ -2,7 +2,7 @@
 Launch the Beard shell if you want to do some experiment with the library.
 """
 
-from cli.beard_shell import BeardShell
+import cli
 
 if __name__ == "__main__": 
-    BeardShell().cmdloop()
+    cli.BeardShell().cmdloop()
---
+++ pos/pos.py
@@ -0,0 +1,2 @@
+class PartOfSpeech:
+    pass
